9c79a93af827a0e5c923cd98064c4978cd49fada
Mark Ryall <mark@ryall.name>
2011-01-14 01:18:23 +1100

    renamed over_18 field

--- db/migrate/20110113133755_add_smoking_over_18.rb
+++ db/migrate/20110113133755_add_smoking_over_18.rb
@@ -1,11 +1,11 @@
 class AddSmokingOver18 < ActiveRecord::Migration
   def self.up
-    add_column :accommodations, :over_18, :boolean
+    add_column :accommodations, :over_eighteen, :boolean
     add_column :accommodations, :takes_smokers, :boolean
   end
 
   def self.down
-    remove_column :accommodations, :over_18, :boolean
+    remove_column :accommodations, :over_eighteen, :boolean
     remove_column :accommodations, :takes_smokers, :boolean
   end
 end
