4d5619b677c300fb7c24181e7460eb752a20be7d
Mark Ryall <mark@ryall.name>
2011-01-14 12:42:18 +1100

    fixed validation of over 18

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,3 +1,3 @@
 class Accommodation < ActiveRecord::Base
-  validates_acceptance_of :over_eighteen, :message => 'must be over 18'
+  validates_acceptance_of :over_eighteen, :accept=>true, :message => 'must be over 18'
 end
