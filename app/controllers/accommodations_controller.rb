7c4b9eae5fa7248cacd266cc097219cfa63fd577
Mark Ryall <mark@ryall.name>
2011-01-14 12:02:03 +1100

    the triumphant return of the destroy action

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -82,13 +82,13 @@ class AccommodationsController < ApplicationController
 
   # DELETE /accommodations/1
   # DELETE /accommodations/1.xml
-  #def destroy
-  #  @accommodation = Accommodation.find(params[:id])
-  #  @accommodation.destroy
-
-  #  respond_to do |format|
-  #    format.html { redirect_to(accommodations_url) }
-  #    format.xml  { head :ok }
-  #  end
-  #end
+  def destroy
+    @accommodation = Accommodation.find(params[:id])
+    @accommodation.destroy
+  
+    respond_to do |format|
+      format.html { redirect_to(accommodations_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
