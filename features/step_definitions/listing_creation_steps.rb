ffaf3079cde4b330c8553f13ba5d69c85d3029e7
duana <strongcoffee@gmail.com>
2011-01-15 20:15:20 +1000

    got first step working of create listing feature spec - that was a nightmare

--- features/step_definitions/listing_creation_steps.rb
+++ features/step_definitions/listing_creation_steps.rb
@@ -1,3 +1,10 @@
-Given /^that no listings have been posted$/ do
-   # don't need to do anything
+Given /^I fill in details about the accommodation I can offer$/ do |table|
+  listing = table.rows_hash
+  goto(CreateListingPage) do |page|
+    page.create_listing listing 
+  end
+end
+
+Then /^I will see confirmation$/ do
+  pending # express the regexp above with the code you wish you had
 end
