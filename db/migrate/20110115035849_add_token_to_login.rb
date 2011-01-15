e1fbd5c66a1c43cd481725c4397196a2c20c74e3
Phillip Calcado <pcalcado@gmail.com>
2011-01-15 15:50:28 +1100

    migratin gone; oops

--- db/migrate/20110115035849_add_token_to_login.rb
+++
@@ -1,9 +0,0 @@
-class AddTokenToLogin < ActiveRecord::Migration
-  def self.up
-      add_column :logins, :authentication_token, :string
-  end
-
-  def self.down
-    remove_column :logins, :authentication_token, :string
-  end
-end
