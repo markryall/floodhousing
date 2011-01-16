5b346c82286055e1bc41da66c8de8c740e7813de
duana <strongcoffee@gmail.com>
2011-01-16 15:59:00 +1000

    added please select to select options for number of beds and area

--- features/support/blueprints.rb
+++ features/support/blueprints.rb
@@ -6,7 +6,8 @@ Accommodation.blueprint do
   address1 {'1/509 Factory'}
   address2 {'Street'}
   suburb  {'FactoryVille'}
-  number_of_beds {1}
+  area  {'Ipswich'}
+  number_of_beds {"1"}
   email {"foo@bar.com"}
   over_eighteen {true}
   terms_and_conditions_accepted {true}
