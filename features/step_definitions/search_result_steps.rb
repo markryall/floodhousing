a7ac804528ca37ee4dad04c0af734bd470a5d64b
duana <strongcoffee@gmail.com>
2011-01-16 18:23:09 +1000

    filter: change number of beds to selection

--- features/step_definitions/search_result_steps.rb
+++ features/step_definitions/search_result_steps.rb
@@ -34,3 +34,13 @@ Then /^I will see the listings in the following order$/ do |table|
   
   actual_order.should eql expected_order
 end
+
+When /^I filter by (\d+) beds$/ do | number_of_beds |
+  on(SearchPage) do | page |
+    page.select_number_of_beds number_of_beds
+  end
+end
+
+Then /^the listing "([^"]*)" will be returned$/ do |listing|
+  @listings.has_key?(listing).should be_true
+end
--- features/support/pages/search_page.rb
+++ features/support/pages/search_page.rb
@@ -16,5 +16,8 @@ class SearchPage < Page
   def pagination_control
     driver.all(".pagination").first
   end
-  
+
+  def select_number_of_beds number_of_beds
+    driver.select number_of_beds, :from => "number_of_beds"
+  end
 end
