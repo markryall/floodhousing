38e3b369cf10f28565d1112fbb0522e7f4f662a0
Brady Lill <bradylill@gmail.com>
2011-01-16 23:12:23 +0900

    Number of people is now number of beds in host contact form.

--- app/models/seeker.rb
+++ app/models/seeker.rb
@@ -12,14 +12,14 @@ class Seeker
     @backup_phone = params[:backup_phone] || ''
     @email = params[:email] || ''
     @contact_method = params[:contact_method] || ''
-    @number_of_people = params[:number_of_people] || ''
+    @number_of_beds = params[:number_of_beds] || ''
     @allergies = params[:allergies] || ''
     @special_requirements = params[:special_requirements] || ''
     @pets =  params[:pets] || ''
     @personal_message = params[:personal_message] || ''
   end
   
-  attr_accessor :name, :phone, :backup_phone, :email, :contact_method, :number_of_people, :allergies, :special_requirements, :pets, :personal_message
+  attr_accessor :name, :phone, :backup_phone, :email, :contact_method, :number_of_beds, :allergies, :special_requirements, :pets, :personal_message
   
   def persisted?
     false
