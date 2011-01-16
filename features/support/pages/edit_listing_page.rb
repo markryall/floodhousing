88758ef2b4db3e0f7bc33f7a90dc7fa4d8b7fc5c
Perryn Fowler <perryn.fowler@gmail.com>
2011-01-16 17:14:18 +1100

    <passiveaggressive>for the love of God,  WRITE TESTS</passiveaggresive>

---
+++ features/support/pages/edit_listing_page.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + "/page"
+class EditListingPage < Page
+    
+   def check_we_are_on_this_page 
+     raise "Expected to be on the edit page, but was not" unless driver.has_css?("h2",:text => "Editing accommodation")  
+   end
+   
+   def address_1
+     driver.find("form #accommodation_address1").value
+   end
+   
+end
