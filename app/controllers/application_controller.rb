e178fccc6974e9cd1d8c74dd38deddb081d5dd48
Julio Maia <julio.maia@gmail.com>
2011-01-18 11:46:29 +1100

    added test.host to the list of hosts that are not redirected by https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :redirect_to_ssl
   
   def redirect_to_ssl
-      redirect_to url_for params.merge({:protocol => 'https://', :host => 'www.ozfloodhelp.org'}) unless (request.ssl? || request.host=='127.0.0.1' || request.host=='localhost')
+      redirect_to url_for params.merge({:protocol => 'https://', :host => 'www.ozfloodhelp.org'}) unless (request.ssl? || request.host=='127.0.0.1' || request.host=='localhost' || request.host=='test.host')
   end
 end
