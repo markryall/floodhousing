a89b6cf24b2d3d31e06416a38863d771bacc852d
duana <strongcoffee@gmail.com>
2011-01-19 03:10:54 +1000

    fix prod email url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Flood::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = {:host => "www.ozfloodhelp.org.au"}
+  config.action_mailer.default_url_options = {:host => "www.ozfloodhelp.org", :protocol => "https"}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
