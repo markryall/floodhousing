4c19be3faa70cd0b0ee41b7f4f17dd4e66a72081
Julio Maia <julio.maia@gmail.com>
2011-01-18 11:39:30 +1100

    forcing https for production environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :redirect_to_ssl
+  
+  def redirect_to_ssl
+      redirect_to url_for params.merge({:protocol => 'https://', :host => 'www.ozfloodhelp.org'}) unless (request.ssl? || request.host=='127.0.0.1' || request.host=='localhost')
+  end
 end
