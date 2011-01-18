f82a944b3d0fc5edb10de634959b67515d09ca4a
Mark Ryall <mark@ryall.name>
2011-01-18 11:41:35 +1100

    added https for email urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Flood::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  config.action_mailer.default_url_options = {:host => "ozfloodhelp.org"}
+
+  config.action_mailer.default_url_options = {:host => "www.ozfloodhelp.org", :protocol => 'https'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
