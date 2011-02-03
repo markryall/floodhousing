6825955b7c39b7f8cbf471f9ab355264535fbdec
duana <strongcoffee@gmail.com>
2011-02-03 14:11:51 +1000

    use env variables for the ssl redirect filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  CANONICAL_HOST = 'www.ozfloodhelp.org'
+  CANONICAL_HOST = ENV['CANONICAL_HOST']
   
   before_filter :redirect_to_ssl
   
   def redirect_to_ssl
-    return unless Rails.env.production?
+    return unless ENV['USE_SSL'] == "true"
     return if request.ssl? && request.host == CANONICAL_HOST 
     redirect_to url_for params.merge({:protocol => 'https://', :host => CANONICAL_HOST})
   end
