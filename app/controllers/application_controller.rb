a2ec4e2fced021b946a3abf02762813d1a577fee
Mark Ryall <mark@ryall.name>
2011-02-06 10:51:47 +1000

    skipping redirection when canonical host is not set - DISABLE_REDIRECTS can be deprecated now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def redirect_to_ssl
     return if ENV['DISABLE_REDIRECTS'] == 'true'
     canonical_host = ENV['CANONICAL_HOST']
+    return unless canonical_host
     wants_ssl = (ENV['USE_SSL'] == 'true')
     return if (request.ssl? || !wants_ssl) && request.host == canonical_host
     redir_scheme = wants_ssl ? "https://" : request.protocol
